335bf561f0697536be1673068ac44109b9615f1f
Jeremy <jere.arde@gmail.com>
2024-12-15 21:12:50 -0800

    init new personal site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://jeremyarde.com",
+  base: "personal-site-2025",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
