419a86d00a34700a8bcd0290aa3bef4535a99e82
Jeremy <jere.arde@gmail.com>
2024-12-15 22:02:14 -0800

    make things simpler

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jeremyarde.com",
-  base: "personal-site-2025",
+  base: "",
   integrations: [mdx(), sitemap(), tailwind()],
 });
